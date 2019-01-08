948cabda3085f7111b6908921c4d9e23841eb209
andric <you@example.com>
2019-01-08 20:57:23 +0300

    git
    git

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+
+var btn = document.querySelector('.section');
+var nav = document.getElementById('nav');
+
+btn.addEventListener('click', function() {
+    nav.classList.toggle('active');
+});
+
+
+
