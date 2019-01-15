caf296b188a9da65a06dc239dd10ef14a4dfca48
andric <you@example.com>
2019-01-15 20:18:15 +0300

    slider
    slider

--- js/slider.js
+++ js/slider.js
@@ -5,15 +5,18 @@ $(document).ready(function(){
     items:1,
     loop:true,
     autoplay:true,
-    autoplayTimeout:3000,
+    autoplayTimeout:4000,
     margin:2,
     mouseDrag:true,
     merge:true,
     slideTransition:'.3s'
+    smartSpeed: 2500
   });
-  $(".btn-nav").on("click", function(){
+
+ /* $(".btn-nav").on("click", function(){
     var target = $(this).data("target");
     $(target).toggleClass("nav__list--open")
   });
 
 });
+*/
