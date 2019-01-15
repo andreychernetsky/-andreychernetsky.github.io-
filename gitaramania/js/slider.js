41dd904052db1aaa6d1d32d984584c42f02f821e
andric <you@example.com>
2019-01-15 20:41:20 +0300

    foto slider
    fotoslider

--- gitaramania/js/slider.js
+++ gitaramania/js/slider.js
@@ -11,9 +11,9 @@ $(document).ready(function(){
     merge:true,
     slideTransition:'.3s'
   });
-  $(".btn-nav").on("click", function(){
+  /*$(".btn-nav").on("click", function(){
     var target = $(this).data("target");
     $(target).toggleClass("nav__list--open")
-  });
+  });*/
 
 });
--- js/slider.js
+++ js/slider.js
@@ -5,18 +5,15 @@ $(document).ready(function(){
     items:1,
     loop:true,
     autoplay:true,
-    autoplayTimeout:4000,
+    autoplayTimeout:3000,
     margin:2,
     mouseDrag:true,
     merge:true,
     slideTransition:'.3s'
-    smartSpeed: 2500
   });
-
- /* $(".btn-nav").on("click", function(){
+  /*$(".btn-nav").on("click", function(){
     var target = $(this).data("target");
     $(target).toggleClass("nav__list--open")
-  });
+  });*/
 
 });
-*/
